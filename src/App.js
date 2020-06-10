b509854e07d344a9d70f42fd4a1fb0f5c330e408
Manish <kohlimanish63@gmail.com>
2020-06-10 10:41:33 +0530

    Fetch Data from Authenticated Github API and created enviorement variables

--- src/App.js
+++ src/App.js
@@ -1,16 +1,32 @@
 import React, { Component } from 'react';
 import Navbar from "./components/layouts/Navbar"
 import Users from "./components/users/users";
+import axios from "axios";
 import './App.css';
 
 class App extends Component {
+  state = {
+    users: [],
+    loading: false
+  }
+
+  async componentDidMount() {
+    console.log(process.env.REACT_APP_GITHUB_SECRET);
+
+    this.setState({ loading: true });
+    const res = await axios.get(`https://api.github.com/users?client_id=${process.env.REACT_APP_GITHUB_ID}&
+    client_secret=${process.env.REACT_APP_GITHUB_SECRET}`);
+
+    this.setState({ users: res.data, loading: false })
+
+  }
 
   render() {
     return (
       <div className="App" >
         <Navbar title='Github Finder' icon='fab fa-github' />
         <div className='container'>
-          <Users />
+          <Users loading={this.state.loading} users={this.state.users} />
         </div>
       </div>
     );
